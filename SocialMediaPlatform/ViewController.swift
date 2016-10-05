0e7a89cef6b5e2f4969b32a7d4ba88fb3b0baa58
Arron Mollet <arron_mollet22@yahoo.com>
2016-10-04 17:03:02 -0700

    Initial Commit

---
+++ SocialMediaPlatform/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialMediaPlatform
+//
+//  Created by Arron Mollet on 10/4/16.
+//  Copyright © 2016 Arron Mollet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
